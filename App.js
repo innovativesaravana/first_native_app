1e90b2e7f4339c87af24b01947c941789a137d41
Saravana Kumar <saravana.kumar@drylogics.com>
2019-01-04 00:06:14 +0530

    Modal css updated

--- App.js
+++ App.js
@@ -63,53 +63,55 @@ class Calendar extends React.Component {
 
   render() {
     return (
-      <Modal style={styles.subContainer}>
-        <View style={styles.headerRow}>
+      <Modal style={styles.modalContainer} transparent={true}>
+        <View style={styles.mainContainer}>
+          <View style={styles.headerRow}>
+            <TouchableOpacity
+              onPress={() => this.leftClicked()}
+              style={styles.headerCells}
+              key="left"
+            >
+              <Text style={styles.textCell}>{"<<"}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.headerCells}
+              onPress={() => this.yearMode()}
+              key="label"
+            >
+              <Text style={styles.textCell}>{this.state.headerLabel}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.rightClicked()}
+              style={styles.headerCells}
+              key="right"
+            >
+              <Text style={styles.textCell}>{">>"}</Text>
+            </TouchableOpacity>
+          </View>
+          {/* <Text>
+          {this.state.selectedMonth} - {this.state.selectedYear}
+        </Text> */}
+          <View style={styles.cellContainer}>
+            {_.map(this.state.cells, cell => {
+              return (
+                <TouchableOpacity
+                  style={styles.monthCells}
+                  onPress={() => this.cellClicked(cell)}
+                  key={cell}
+                >
+                  <Text style={styles.cellText}>{cell}</Text>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
           <TouchableOpacity
-            onPress={() => this.leftClicked()}
-            style={styles.headerCells}
-            key="left"
-          >
-            <Text>{"<<"}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.headerCells}
-            onPress={() => this.yearMode()}
+            style={styles.closeButton}
+            onPress={() => this.props.closeModel()}
             key="label"
           >
-            <Text>{this.state.headerLabel}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => this.rightClicked()}
-            style={styles.headerCells}
-            key="right"
-          >
-            <Text>{">>"}</Text>
+            <Text style={styles.textCell}>Close</Text>
           </TouchableOpacity>
         </View>
-        <Text>
-          {this.state.selectedMonth} - {this.state.selectedYear}
-        </Text>
-        <View style={styles.cellContainer}>
-          {_.map(this.state.cells, cell => {
-            return (
-              <TouchableOpacity
-                style={styles.monthCells}
-                onPress={() => this.cellClicked(cell)}
-                key={cell}
-              >
-                <Text>{cell}</Text>
-              </TouchableOpacity>
-            );
-          })}
-        </View>
-        <TouchableOpacity
-          style={styles.headerCells}
-          onPress={() => this.props.closeModel()}
-          key="label"
-        >
-          <Text>Close</Text>
-        </TouchableOpacity>
       </Modal>
     );
   }
@@ -269,8 +271,8 @@ const styles = StyleSheet.create({
     flex: 1
   },
   yearButton: {
-    width: 100,
-    height: 50,
+    width: "30%",
+    height: "20%",
     alignItems: "center",
     borderColor: "black",
     borderWidth: 2,
@@ -278,8 +280,8 @@ const styles = StyleSheet.create({
     alignSelf: "stretch"
   },
   monthButton: {
-    width: 100,
-    height: 50,
+    width: "30%",
+    height: "25%",
     alignItems: "center",
     borderColor: "black",
     borderWidth: 2,
@@ -287,28 +289,40 @@ const styles = StyleSheet.create({
     alignSelf: "stretch"
   },
   subContainer: {
-    width: 300,
-    height: 330,
-    marginTop: 50,
+    backgroundColor: "black",
+    width: "10%",
+    height: "10%",
+    marginTop: "10%",
     marginLeft: "10%",
+    alignItems: "center",
+    borderColor: "black",
+    borderWidth: 5
+  },
+  modalContainer: {
+    borderWidth: 5
+  },
+  mainContainer: {
+    backgroundColor: "white",
+    width: "70%",
+    height: "60%",
+    marginTop: "30%",
+    marginLeft: "5%",
     alignItems: "center",
     borderColor: "black",
     borderWidth: 5
   },
   cellContainer: {
-    width: 300,
-    height: 280,
-    marginTop: 50,
+    width: "100%",
+    height: "80%",
+    marginTop: "2%",
     flexDirection: "row",
     flexWrap: "wrap",
-    marginLeft: "10%",
-    alignItems: "center",
-    borderColor: "black",
-    borderWidth: 5
+    marginLeft: "1%",
+    alignItems: "center"
   },
   row: {
-    width: 200,
-    height: 50,
+    width: "80%",
+    height: "5%",
     alignItems: "center",
     borderColor: "black",
     borderWidth: 2,
@@ -316,15 +330,16 @@ const styles = StyleSheet.create({
     alignSelf: "stretch"
   },
   headerRow: {
-    width: 300,
-    height: 25,
+    width: "100%",
+    height: "10%",
+    marginTop: "5%",
     alignItems: "center",
     flexDirection: "row",
     alignSelf: "stretch"
   },
   monthCells: {
     width: "33%",
-    height: "20%",
+    height: "25%",
     alignItems: "center",
     alignSelf: "stretch",
     borderColor: "black",
@@ -333,13 +348,27 @@ const styles = StyleSheet.create({
   headerCells: {
     width: "33%",
     height: "100%",
+    textAlign: "center",
     alignItems: "center",
     alignSelf: "stretch"
   },
+  textCell: {
+    marginTop: "15%"
+  },
+  cellText: {
+    marginTop: "25%"
+  },
+  closeButton: {
+    marginBottom: "2%",
+    width: "15%",
+    height: "8%",
+    borderColor: "black",
+    borderWidth: 2
+  },
   viewButton: {
     width: "33%",
-    height: "100%",
-    marginTop: 50,
+    height: "20%",
+    marginTop: "10%",
     alignItems: "center",
     alignSelf: "stretch"
   }
